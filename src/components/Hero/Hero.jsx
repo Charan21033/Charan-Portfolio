9879ec3b3df7e828311e7e05162a98e6fe285eb3
Charan21033 <chinu007r@gmail.com>
2024-03-14 14:12:57 +0530

    hero image change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -17,7 +17,7 @@ export const Hero = () => {
         </a>
       </div>
         <img
-        src={getImageUrl("hero/charan.png")}
+        src={getImageUrl("hero/charan1.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
